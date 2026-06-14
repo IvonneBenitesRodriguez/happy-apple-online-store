c4063ef9d4cd178ac8037ef68aaebcfd541bfe89
Ivonne Benites <ivonnebenites2015@gmail.com>
2026-06-14 20:44:26 +0200

    Add cart_items_controller.rb file

---
+++ app/controllers/cart_items_controller.rb
@@ -0,0 +1,25 @@
+class CartItemsController < ApplicationController
+  def create
+    @cart = current_user.cart || current_user.create_cart
+    product = Product.find(params[:product_id])
+
+    item = @cart.cart_items.find_or_initialize_by(product: product)
+    item.quantity = (item.quantity || 0) + 1
+    item.save
+
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_to products_path }
+    end
+  end
+
+  def destroy
+    item = current_user.cart.cart_items.find(params[:id])
+    item.destroy
+
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_to products_path }
+    end
+  end
+end
